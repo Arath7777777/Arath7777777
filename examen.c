5c6458e9d49e66d92ff5308dd4889209ae7e7dc6
Arath <jeltytimbers547@gmail.com>
2024-11-19 18:20:08 -0800

    jajajaja

---
+++ examen.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+    // Aquí va el código
+    printf("¡Hola, Mundo!\n");  // Ejemplo de impresión en consola
+    return 0;  // Indica que el programa finalizó correctamente
+}
